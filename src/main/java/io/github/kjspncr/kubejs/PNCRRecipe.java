4a1886c2f222662efd506e6fa447de03e5a44030
omgimanerd <alvin@omgimanerd.tech>
2024-04-30 02:21:12 -0400

    Rename PneumaticraftInputFluid.java to PNCRInputFluid.java

--- src/main/java/io/github/kjspncr/kubejs/PNCRRecipe.java
+++ src/main/java/io/github/kjspncr/kubejs/PNCRRecipe.java
@@ -11,7 +11,7 @@ import dev.latvian.mods.kubejs.item.ingredient.IngredientJS;
 import dev.latvian.mods.kubejs.recipe.RecipeJS;
 import dev.latvian.mods.kubejs.recipe.RecipesEventJS;
 import dev.latvian.mods.kubejs.util.ConsoleJS;
-import io.github.kjspncr.kubejs.utils.PneumaticcraftInputFluid;
+import io.github.kjspncr.kubejs.utils.PNCRInputFluid;
 import me.desht.pneumaticcraft.api.crafting.ingredient.FluidIngredient;
 import net.minecraft.util.GsonHelper;
 import net.minecraftforge.fluids.FluidStack;
@@ -51,7 +51,7 @@ public class PNCRRecipe extends RecipeJS {
 
     @Override
     public JsonElement writeInputFluid(InputFluid value) {
-        if (value instanceof PneumaticcraftInputFluid fluid) {
+        if (value instanceof PNCRInputFluid fluid) {
             return fluid.ingredient().toJson();
         } else if (value instanceof FluidIngredient fluid) {
             return fluid.toJson();
@@ -65,20 +65,24 @@ public class PNCRRecipe extends RecipeJS {
 
     @Override
     public InputFluid readInputFluid(Object from) {
-        if (from instanceof PneumaticcraftInputFluid fluid) {
+        if (from instanceof PNCRInputFluid fluid) {
             return fluid;
         } else if (from instanceof FluidIngredient fluid) {
-            return new PneumaticcraftInputFluid(fluid);
+            return new PNCRInputFluid(fluid);
         } else if (from instanceof FluidStack fluid) {
-            return new PneumaticcraftInputFluid(FluidIngredient.of(fluid));
+            return new PNCRInputFluid(FluidIngredient.of(fluid));
         } else if (from instanceof FluidStackJS fluid) {
             FluidIngredient f = FluidIngredient.of(FluidStackHooksForge.toForge(fluid.getFluidStack()));
-            return new PneumaticcraftInputFluid(f);
+            return new PNCRInputFluid(f);
         } else if (from instanceof JsonObject j) {
+            // TODO: this is potentially problematic because this calls
+            // ForgeRegistries.FLUIDS and the fluid registry is not available at startup
+            // This causes some recipe parsing errors on first load, which disappear after
+            // a /reload. As a hack, users can force call /reload when the server starts lol
             FluidIngredient f = FluidIngredient.Serializer.INSTANCE.parse(j);
-            return new PneumaticcraftInputFluid(f);
+            return new PNCRInputFluid(f);
         }
         ConsoleJS.SERVER.warn("Unknown input fluid %s of type %s".formatted(from, from.getClass()));
-        return PneumaticcraftInputFluid.EMPTY;
+        return PNCRInputFluid.EMPTY;
     }
 }
